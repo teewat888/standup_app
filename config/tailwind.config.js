81b4e37b2c2d6d94bc9ab5bd1d689509c3d3b28e
Teerawat P. <teewat@yahoo.com>
2024-02-13 21:02:06 +1100

    Devise signup and error helper

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,11 +1,25 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
+  safelist: [
+    'bg-orange-100',
+    'border-orange-400',
+    'text-orange-700',
+    'bg-red-100',
+    'border-red-400',
+    'text-red-700',
+    'bg-yellow-100',
+    'border-yellow-400',
+    'text-yellow-700',
+    'bg-green-100',
+    'border-green-400',
+    'text-green-700',
+  ],
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
   ],
   theme: {
     extend: {
@@ -19,5 +33,5 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-  ]
-}
+  ],
+};
